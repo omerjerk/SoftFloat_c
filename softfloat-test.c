b3911de9a1d4544c0b20895d23181157916b0ecc
Umair Khan <omerjerk@gmail.com>
2016-06-17 22:27:22 +0530

    improve tests
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- softfloat-test.c
+++ softfloat-test.c
@@ -4,10 +4,10 @@
 
 int main() {
 	int32_t a, b, c;
-	a = 10;
-	
+	a = -10;
+
 	SoftFloat a_sf = int2sf(a);
-	printf("Normal fields check for %d\n", a);
+	printf("Normal fields check for %d:\n", a);
 	printf("sign = %d\n", a_sf.sign);
 	printf("exp = %d\n", a_sf.exp);
 	printf("mant = %d\n", a_sf.mant);
@@ -17,10 +17,17 @@ int main() {
 	c = -10;
 	SoftFloat b_sf = int2sf(b);
 	SoftFloat c_sf = int2sf(c);
-	SoftFloat prod = mul_sf(b_sf, c_sf);
-	printf("Multiplication check for %d and %d\n", b, c);
-	printf("sign = %d\n", prod.sign);
-	printf("exp = %d\n", prod.exp);
-	printf("mant = %d\n", prod.mant);
+	a_sf = mul_sf(b_sf, c_sf);
+	printf("Multiplication check for %d and %d:\n", b, c);
+	printf("sign = %d\n", a_sf.sign);
+	printf("exp = %d\n", a_sf.exp);
+	printf("mant = %d\n", a_sf.mant);
+	printf("========================\n\n");
+
+	a_sf = div_sf(b_sf, c_sf);
+	printf("Division check for %d by %d:\n", b, c);
+	printf("sign = %d\n", a_sf.sign);
+	printf("exp = %d\n", a_sf.exp);
+	printf("mant = %d\n", a_sf.mant);
 	printf("========================\n\n");
 }
