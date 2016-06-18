e81977981184595bf450f9d56543712327af6f6c
Umair Khan <omerjerk@gmail.com>
2016-06-18 10:52:12 +0530

    Add bits2sf function
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- softfloat.h
+++ softfloat.h
@@ -3,12 +3,12 @@
 
 #include <stdint.h>
 
-#define EXP_BIAS -127
+#define EXP_BIAS 127
 #define MANT_BITS 23
 
 typedef struct SoftFloat {
     int32_t sign;
-    uint32_t mant;
+    uint64_t mant;
     int32_t  exp;
 } SoftFloat;
 
@@ -26,9 +26,7 @@ static SoftFloat normalize_sf(SoftFloat sf) {
 
 static SoftFloat int2sf(int32_t n, int e) {
     int sign = 0;
-    int32_t exp;
     uint32_t mant;
-    SoftFloat sf;
 
     if (n < 0) {
         sign = 1;
@@ -36,6 +34,10 @@ static SoftFloat int2sf(int32_t n, int e) {
     }
     mant = n << 23; //Keep mant in a 64-bit integer may be ?
     return normalize_sf((SoftFloat) {sign, mant, 0 + e});
+}
+
+static SoftFloat bits2sf(uint32_t n) {
+    return ((SoftFloat) { (n & 0x80000000UL), (n & 0x7F800000UL), (n & 0x7FFFFFUL) });
 }
 
 static int sf2int(SoftFloat a) {
@@ -59,12 +61,12 @@ static SoftFloat mul_sf(SoftFloat a, SoftFloat b) {
     a = normalize_sf(a);
     b = normalize_sf(b);
     sign = a.sign ^ b.sign;
-    mant = (uint32_t) (((uint64_t)(a.mant|0x00800000UL) * (uint64_t)(b.mant|0x00800000UL))>>MANT_BITS);
+    mant = (((uint64_t)(a.mant|0x00800000UL) * (uint64_t)(b.mant|0x00800000UL))>>MANT_BITS);
     exp = a.exp + b.exp;
     return normalize_sf((SoftFloat) {sign, mant, exp});
 }
 
-static int eq_sf(SoftFloat a, SoftFloat b) {
+static int cmp_sf(SoftFloat a, SoftFloat b) {
     a = normalize_sf(a);
     b = normalize_sf(b);
     if (a.sign != b.sign) return 0;
